2225dea78a05c4cb9ec5cf7eafbb6c0903f4e23f
boguan <786506@gmail.com>
2014-06-01 22:37:20 +0800

    add support source map

--- index.js
+++ index.js
@@ -9,21 +9,34 @@
 var less = require('less');
 var root = fis.project.getProjectPath();
 
-module.exports = function(content, file, conf){
-    conf.paths = [ file.dirname, root ];
-    conf.syncImport = true;
-    var parser = new(less.Parser)(conf);
-    parser.parse(content, function (err, tree) {
-        if(err){
-            throw err;
-        } else {
-            if(parser.imports){
-                fis.util.map(parser.imports.files, function(path){
-                    file.cache.addDeps(path);
-                });
-            }
-            content = tree.toCSS(conf);
-        }
-    });
+module.exports = function(content, file, conf) {
+	conf.paths = [file.dirname, root];
+	conf.syncImport = true;
+
+
+	// 参考 https://github.com/gruntjs/grunt-contrib-less/blob/master/README.md
+	conf.filename = '/source' + file.subpath; // *.map {sources: '此处用到'}
+	conf.sourceMap = true;
+	conf.sourceMapFilename = file.filename + '.map';
+	conf.sourceMapURL = file.filename + '.map';
+	conf.writeSourceMap = function(sourceMapContent) {
+		var fs = require('fs');
+		fs.writeFileSync(file.dirname + '/' + file.filename + '.map', sourceMapContent, null);
+	};
+
+
+	var parser = new(less.Parser)(conf);
+	parser.parse(content, function(err, tree) {
+		if (err) {
+			throw err;
+		} else {
+			if (parser.imports) {
+				fis.util.map(parser.imports.files, function(path) {
+					file.cache.addDeps(path);
+				});
+			}
+			content = tree.toCSS(conf);
+		}
+	});
 	return content;
 };
